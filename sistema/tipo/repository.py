9167b72c39508bec1e62996eb4102e5e58e54342
Hadson00 <hadson798@gmail.com>
2024-08-28 19:49:28 -0300

    Alteracao do repository de tipo e users

--- sistema/tipo/repository.py
+++ sistema/tipo/repository.py
@@ -6,9 +6,13 @@ class TypeRepository:
         return Type.objects.all()
     
     @staticmethod
+    def get_id_type(type_id):
+        return Type.objects.get(id = type_id)
+    
+    @staticmethod
     def delete_type(type_id):
         try:
-            type = Type.objects.get(id=type_id)
+            type = TypeRepository.get_id_type(id = type_id)
             type.delete()
             return True
         except Type.DoesNotExist:
@@ -21,22 +25,22 @@ class TypeRepository:
     @staticmethod
     def update_type(type_id, name):
         try:
-            type = Type.objects.get(id=type_id)
+            type = TypeRepository.get_id_type(id = type_id)
             type.name = name
             type.save()
             return True
         except Type.DoesNotExist:
             return False
         
-    @staticmethod
-    def search_type(query):
-        try:
-            # Primeiro tenta buscar por ID
-            if query.isdigit():
-                return Type.objects.filter(id=query)
-        except ValueError:
-            pass
+    # @staticmethod
+    # def search_type(query):
+    #     try:
+    #         # Primeiro tenta buscar por ID
+    #         if query.isdigit():
+    #             return Type.objects.filter(id=query)
+    #     except ValueError:
+    #         pass
         
-        # Se não for um ID válido, busca por nome (convertendo para maiúsculas)
-        query_upper = query.upper()
-        return Type.objects.filter(name__icontains=query_upper)
+    #     # Se não for um ID válido, busca por nome (convertendo para maiúsculas)
+    #     query_upper = query.upper()
+    #     return Type.objects.filter(name__icontains=query_upper)
