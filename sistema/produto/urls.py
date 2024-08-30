01644296f0faddb1d485a14afed48117bf9f1515
Igor alves SENAI <igorribeiroalves56@gmail.com>
2024-08-29 22:05:50 -0300

    mural-comments template

--- sistema/produto/urls.py
+++ sistema/produto/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('produto/<int:product_id>', Product_Single.as_view(), name='product_single'),
     path('criar/', CreateProduct.as_view(), name='create_product'),
     path('editar/<int:product_id>', UpdateProduct.as_view(), name='update_product'),
-    path('deletar/<int:product_id>', DeleteProduct.as_view(), name='delete_product')
+    path('deletar/<int:product_id>', DeleteProduct.as_view(), name='delete_product'),
+    path('mural/', CommentPageList.as_view(), name='mural_comment')
 ]
