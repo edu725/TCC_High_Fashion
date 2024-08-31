aceae9b04513a47cec1bbd1fc229cc936f42da70
RaphaZY <raphacoelho3790@gmail.com>
2024-08-31 11:58:33 -0300

    Comentario

--- sistema/produto/urls.py
+++ sistema/produto/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('criar/', CreateProduct.as_view(), name='create_product'),
     path('editar/<int:product_id>', UpdateProduct.as_view(), name='update_product'),
     path('deletar/<int:product_id>', DeleteProduct.as_view(), name='delete_product'),
-    path('mural/', CommentPageList.as_view(), name='mural_comment')
+    path('mural/', CommentPageList.as_view(), name='mural_comment'),
+    path('criar/comentario/<int:user_id>', CreateCommentPage.as_view(), name='create_comment_page'),  
 ]
